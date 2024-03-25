a66a39d8a3b8de439d28864731a89e1a4b76ebed
stsosupa <stina.s.s.palomaki@student.jyu.fi>
2024-03-25 17:13:32 +0200

    harjoittelua vaa

--- osa1/harjoittelu/part1/src/App.jsx
+++ osa1/harjoittelu/part1/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Hello = (props) => {
   const arr = ["kissa", "koira", "apina", "sika", "hevonen"];
   const index = Math.floor(Math.random() * arr.length);
@@ -9,12 +11,45 @@ const Hello = (props) => {
   );
 };
 
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div> the app is used by pressing the buttons </div>;
+  }
+  return <div> button press history: {props.allClicks.join(" ")} </div>;
+};
+
 const App = () => {
+  const [zeros, setZeros] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
   const now = new Date();
   const a = 10;
   const b = 20;
   console.log(now, a + b);
 
+  const increaseByOne = () => {
+    setCounter(counter + 1);
+    setTotal(total + 1);
+    setAll(allClicks.concat("1"));
+  };
+  const setToZero = () => {
+    setCounter(0);
+    setZeros(zeros + 1);
+    setTotal(total + 1);
+    setAll(allClicks.concat("0"));
+  };
+
+  // setTimeout(() => setCounter(counter + 1), 1000);
+  console.log(counter);
+  console.log(zeros);
   return (
     <>
       <Hello imperatiivi="pasko" />
@@ -26,6 +61,13 @@ const App = () => {
       <p>
         {a} plus {b} is {a + b}
       </p>
+      <div>
+        <Display counter={counter} />
+        <Button handleClick={increaseByOne} text="plus" />
+        <Button handleClick={setToZero} text="nollaa" />
+        <History allClicks={allClicks} />
+        <p>Total on: {total}</p>
+      </div>
     </>
   );
 };
--- osa1/harjoittelu/part1/src/main.jsx
+++ osa1/harjoittelu/part1/src/main.jsx
@@ -2,4 +2,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+let counter = 1;
+
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
+  <App counter={counter} />
+);
