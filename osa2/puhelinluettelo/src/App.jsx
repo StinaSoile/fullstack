c54db8f6a1cf1310357ddb4f5563820e719fc86b
stsosupa <stina.s.s.palomaki@student.jyu.fi>
2024-03-30 21:12:35 +0200

    2.6 alkuun

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const Person = ({ person }) => {
+  return <li>{person.name}</li>;
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <div>debug: {newName}</div>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <Person key={person.name} person={person} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
